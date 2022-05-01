e0d9b8afa6b521346b057e6bf3be7fc6c53ab40c
Zhdanov Valery <becwal@yandex.ru>
2022-05-02 00:17:51 +0300

    Результат игры

--- knb.js
+++ knb.js
@@ -1,7 +1,15 @@
 function check(par)
 {
     comp_par = Math.floor(Math.random() * 3); 
-    alert("Вы выбрали "+change(par)+" Компьютер выбрал "+change(""+comp_par))
+    result=""
+    if (par==comp_par) result="Ничья"
+    else if (par==0 && comp_par==2) result="Компьютер выиграл"
+    else if (par==2 && comp_par==0) result="Вы выиграли"
+    else if (par>comp_par) result="Компьютер выиграл"
+    else if (par<comp_par) result="Вы выиграли"
+
+
+    alert("Вы выбрали "+change(par)+". Компьютер выбрал "+change(""+comp_par)+". "+result)
 }
 
 function change(par)
