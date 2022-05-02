8fa2d75ea64bb31778142ebf628f51fd12b8f7a4
Zhdanov Valery <becwal@yandex.ru>
2022-05-02 11:32:26 +0300

    Разноцветная результирующая панель

--- knb.js
+++ knb.js
@@ -8,9 +8,18 @@ function check(par)
     else if (par==2 && comp_par==0) result="Вы выиграли"
     else if (par>comp_par) result="Компьютер выиграл"
     else if (par<comp_par) result="Вы выиграли"
+   
+    //окраска окна результата
+    resultclass_name = ""
+    if (result=="Компьютер выиграл")
+        resultclass_name = "alert alert-danger"
+    else if (result=="Вы выиграли")
+        resultclass_name = "alert alert-success"
+    else if (result=="Ничья")    
+        resultclass_name = "alert alert-secondary"
 
-
-    document.getElementById("result").innerHTML = " Вы выбрали "+change(par)+". Компьютер выбрал "+change(""+comp_par)+". "+result
+    document.getElementById("result").className= resultclass_name
+    document.getElementById("result").innerHTML = " Вы выбрали <strong>"+change(par)+"</strong>. Компьютер выбрал <strong>"+change(""+comp_par)+"</strong>. "+result
     
 }
 
