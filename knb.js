bb8588017842497a790b02a782aae71c48efa049
Zhdanov Valery <becwal@yandex.ru>
2022-05-02 12:02:57 +0300

    Карточки результата

--- knb.js
+++ knb.js
@@ -11,16 +11,63 @@ function check(par)
    
     //окраска окна результата
     resultclass_name = ""
+    respict =""
     if (result=="Компьютер выиграл")
-        resultclass_name = "alert alert-danger"
+        {
+            resultclass_name = "alert alert-danger mb-2"
+            respict="comp"
+        }
+        
     else if (result=="Вы выиграли")
-        resultclass_name = "alert alert-success"
-    else if (result=="Ничья")    
-        resultclass_name = "alert alert-secondary"
+    {
+        resultclass_name = "alert alert-success mb-2"
+        respict="man"
+    }
+       
+    else if (result=="Ничья")  
+    {
+        resultclass_name = "alert alert-secondary mb-2"
+        respict="none"
+    }  
+        
 
     document.getElementById("result").className= resultclass_name
-    document.getElementById("result").innerHTML = " Вы выбрали <strong>"+change(par)+"</strong>. Компьютер выбрал <strong>"+change(""+comp_par)+"</strong>. "+result
-    
+    document.getElementById("res_info").innerHTML = "Результат игры"
+
+    res_you = "<div class='card' style='width: 18rem;'> \
+    <img src='image\\"+par+".jpg' class='card-img-top' alt='...'> \
+    <div class='card-body'>\
+      <h5 class='card-title'>Ваш выбор</h5> \
+      <p class='card-text'>"+change(par)+"</p>\
+      \
+    </div> \
+  </div>"
+
+
+  res_comp = "<div class='card' style='width: 18rem;'> \
+  <img src='image\\"+comp_par+".jpg' class='card-img-top' alt='...'> \
+  <div class='card-body'>\
+    <h5 class='card-title'>Выбор компьютера</h5> \
+    <p class='card-text'>"+change(""+comp_par)+"</p>\
+    \
+  </div> \
+</div>"
+
+
+res_win = "<div class='card' style='width: 18rem;'> \
+<img src='image\\"+respict+".png' class='card-img-top' alt='...'> \
+<div class='card-body'>\
+  <h5 class='card-title'>Результат</h5> \
+  <p class='card-text'>"+result+"</p>\
+  \
+</div> \
+</div>"
+    //document.getElementById("result").innerHTML = " Вы выбрали <strong>"+change(par)+"</strong>. Компьютер выбрал <strong>"+change(""+comp_par)+"</strong>. "+result
+   
+    document.getElementById("result_you").innerHTML = res_you
+    document.getElementById("result_comp").innerHTML = res_comp 
+    document.getElementById("result_win").innerHTML = res_win
+
 }
 
 
